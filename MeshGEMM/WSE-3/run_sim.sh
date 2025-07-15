d9364346e5aa9f70f040c47655ea283c4913e151
DD-DuDa <951528763@qq.com>
2025-07-15 16:41:51 +0100

    meshgemm wse-3

---
+++ MeshGEMM/WSE-3/run_sim.sh
@@ -0,0 +1,25 @@
+set -e
+
+P=4
+
+M=16
+N=16
+K=16
+
+Mt=$(($M / $P))
+Kt=$(($K / $P))
+Nt=$(($N / $P))
+
+fabric_w=$(($P + 7))
+fabric_h=$(($P + 2))
+
+cslc --arch=wse3 ./src/layout.csl --fabric-dims="$fabric_w","$fabric_h" --fabric-offsets=4,1 \
+    --params=P:"$P",Mt:"$Mt",Kt:"$Kt",Nt:"$Nt" \
+    -o out --memcpy --channels 1
+
+cs_python ./launch_sim.py --P "$P" --M "$M" --K "$K" --N "$N"
+
+
+rm -rf simfab_traces
+rm *.json
+rm sim.log
